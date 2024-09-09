be6c1081516fd59b8b24662c1ea5d5154fbcb5f8
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-09 10:49:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+
+
+    System.out.println("O primeiro número é:");
+    int numero1 = 4;
+
+
+  
+    System.out.println(numero1);
+ System.out.println("O segundo número é:");
+    int numero2 = 6;
+
+      System.out.println(numero2);
+
+
+    System.out.println("O resultado da soma é igual a:");
+    int soma = numero1 + numero2;
+    System.out.println(soma);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
